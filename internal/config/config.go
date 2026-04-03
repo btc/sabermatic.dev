42c4e793dfaccfef1a0d403b443f48bf5ab8cee5
btc <734339+btc@users.noreply.github.com>
2026-04-03 17:33:53 -0400

    feat(billing): add Stripe config sub-struct

--- internal/config/config.go
+++ internal/config/config.go
@@ -21,6 +21,7 @@ type Config struct {
 	Auth     Auth
 	OAuth    OAuth
 	Otel     Otel
+	Stripe   Stripe
 }
 
 type Server struct {
@@ -114,6 +115,15 @@ type Otel struct {
 	SampleRate   float64 `env:"OTEL_SAMPLE_RATE,default=1.0"`
 	ServiceName  string  `env:"OTEL_SERVICE_NAME,default=drill"`
 	GCPProjectID string  `env:"GOOGLE_CLOUD_PROJECT"`
+}
+
+type Stripe struct {
+	SecretKey      string `env:"STRIPE_SECRET_KEY"`
+	WebhookSecret  string `env:"STRIPE_WEBHOOK_SECRET"`
+	ProPriceID     string `env:"STRIPE_PRO_PRICE_ID"`
+	Pack120PriceID string `env:"STRIPE_PACK_120_PRICE_ID"`
+	Pack300PriceID string `env:"STRIPE_PACK_300_PRICE_ID"`
+	Pack600PriceID string `env:"STRIPE_PACK_600_PRICE_ID"`
 }
 
 // SecureCookies returns true if BaseURL uses HTTPS, indicating cookies
