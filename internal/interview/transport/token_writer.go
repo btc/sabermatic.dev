3339cea4a64a5ba07e09f9fe87448afda00b63bc
btc <734339+btc@users.noreply.github.com>
2026-04-07 00:44:22 -0400

    feat(transport): add TokenWriter and TTSSink adapter

---
+++ internal/interview/transport/token_writer.go
@@ -0,0 +1,26 @@
+package transport
+
+import "github.com/google/uuid"
+
+type TokenWriter struct {
+	client    Client
+	messageID uuid.UUID
+}
+
+func NewTokenWriter(client Client, messageID uuid.UUID) *TokenWriter {
+	return &TokenWriter{client: client, messageID: messageID}
+}
+
+func (w *TokenWriter) OnToken(token string) {
+	w.client.InterviewerToken(token)
+}
+
+func (w *TokenWriter) OnDone(_ string) {
+	w.client.InterviewerDone(w.messageID)
+}
+
+func (w *TokenWriter) OnError(err error) {
+	w.client.Error(ClientError{Code: "llm_stream_error", Message: err.Error()})
+}
+
+func (w *TokenWriter) Interrupt() {}
